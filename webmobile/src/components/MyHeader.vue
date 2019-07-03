e31ca03694e909837304658329f64a74c43dbd4c
hyoya <ghtybndss@gmail.com>
2019-07-03 10:59:13 +0900

    Req. 1-2 :: show title

--- webmobile/src/components/MyHeader.vue
+++ webmobile/src/components/MyHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div style="background-color:red">
-    <h1>This is Test - MyHeader Content</h1>
+  <div style="background-color:red; text-align:center">
+    <h1>Hyoyas</h1>
     <!-- TODO : 내용을 채워야함 -->
   </div>
 </template>
