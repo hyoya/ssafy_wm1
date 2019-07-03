a2ffb1ede0944c2737938e45fa15382da6875ac6
hyoya <ghtybndss@gmail.com>
2019-07-03 18:32:29 +0900

    Req. 1-17 :: Back to top

--- webmobile/src/App.vue
+++ webmobile/src/App.vue
@@ -1,6 +1,21 @@
+<style>
+.btn-to-top {
+  width: 60px;
+  height: 60px;
+  padding: 10px 16px;
+  border-radius: 50%;
+  font-size: 22px;
+  line-height: 22px;
+}
+</style>
+
 <template>
   <v-app>
     <v-content>
+
+      <back-to-top bottom="50px" right="50px">
+  <button type="button" class="btn btn-info btn-to-top"><i class="fa fa-chevron-up"></i></button>
+</back-to-top>
       <!-- Header -->
       <v-layout column>
         <v-flex xs12>
@@ -22,21 +37,24 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import BackToTop from 'vue-backtotop'
 import store from './store'
 import MyHeader from "./components/MyHeader"
 import MyFooter from "./components/MyFooter"
 
 export default {
-	name: 'App',
-	store,
+  name: 'App',
+  store,
   components : {
     MyHeader,
-    MyFooter
+    MyFooter,
+    BackToTop,
   },
-	data() {
-		return {
-			//
-		}
-	}
+  data() {
+    return {
+      //
+    }
+  }
 }
 </script>
