72476bb7bd5fe20dc8d9dcf8d32e0826ec39dc60
hyoya <ghtybndss@gmail.com>
2019-07-03 11:48:34 +0900

    Req. 1-6 :: reactive page - col_row size

--- webmobile/src/components/PortfolioList.vue
+++ webmobile/src/components/PortfolioList.vue
@@ -1,6 +1,7 @@
 <template>
   <v-layout mt-5 wrap>
-    <v-flex v-for="i in portfolios.length > limits ? limits : portfolios.length" xs4>
+
+    <v-flex v-for="i in portfolios.length > limits ? limits : portfolios.length" xs12 sm6>
       <Portfolio class="ma-3"
               :date="portfolios[i - 1].created_at.toString()"
               :title="portfolios[i - 1].title"
@@ -8,12 +9,14 @@
               :imgSrc="portfolios[i - 1].img"
       ></Portfolio>
     </v-flex>
-
     <v-flex xs12 text-xs-center round my-5 v-if="loadMore">
       <v-btn color="info" dark v-on:click="loadMorePortfolios"><v-icon size="25" class="mr-2">fa-plus</v-icon> 더 보기</v-btn>
     </v-flex>
+
   </v-layout>
 </template>
+
+
 <script>
 import Portfolio from '@/components/Portfolio'
 import FirebaseService from '@/services/FirebaseService'
@@ -21,7 +24,7 @@ import FirebaseService from '@/services/FirebaseService'
 export default {
 	name: 'PortfoliosList',
 	props: {
-		limits: {type: Number, default: 3},
+		limits: {type: Number, default: 4},
     loadMore: {type: Boolean, default: false}
 	},
 	data() {
